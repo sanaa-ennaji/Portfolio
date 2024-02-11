2ffbe6ca6d021fb9ed32827a2d348e3445207954
sanaa <sanaaennaji93@gmail.com>
2024-02-11 15:23:07 +0100

    add home page

---
+++ portfolio.js
@@ -0,0 +1,34 @@
+const  navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+
+
+
+if(navToggle){
+navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu')
+})
+}
+
+
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    } )
+}
+
+// remove menu mobile
+const navLink = document.querySelector('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction) )
+    
+const section = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = Window.pageYOffset
+}
